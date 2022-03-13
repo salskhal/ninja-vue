ac1cc09a2f85fcaf91cc3444355deaef7a1450d5
salskhal <olaniyisal@gmail.com>
2022-03-13 10:00:39 +0100

    Feature/ CLI basics

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{title}} </h1>
+  <div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+  </div>
+  <br>
+  <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Model.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components:{
+    Modal
+  }, 
+  data(){
+    return{
+      title: "My first vue app",
+      header: "Sign up for the Giveaway!",
+      text: "Grab your Khamurad fashion swags at half price",
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +39,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
